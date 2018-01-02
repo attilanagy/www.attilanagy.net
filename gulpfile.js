708d82dab9b5ac565f3ded52a889017012115036
Attila Nagy <attila@attilanagy.net>
2018-01-02 17:17:21 +0100

    :Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var browserSynch = require("browser-sync").create(),
+    cleanCSS = require("gulp-clean-css"),
+    del = require("del"),
+    gulp = require("gulp"),
+    sass = require("gulp-sass");
+
+var cssGenerator = gulp.src("src/sass/attilanagy.scss")
+                   .pipe(sass().on("error", sass.logError));
+
+gulp.task("clean", () => {
+  return del([ "build/" ]);
+});
+
+
+gulp.task("html", () => {
+  return gulp.src("src/index.html")
+         .pipe(gulp.dest("build/"));
+});
+
+gulp.task("sass", () => {
+  return cssGenerator
+         .pipe(gulp.dest("build/css"));
+});
+
+gulp.task("dist", [ "html" ], () => {
+  return cssGenerator
+         .pipe(cleanCSS())
+         .pipe(gulp.dest("build/css"));
+});
+
+gulp.task("serve", [ "sass", "html" ], () => {
+  browserSynch.init( { "server": "./build/" });
+  gulp.watch("./src/sass/*.scss", [ "sass" ]);
+  gulp.watch("./src/*.html", [ "html" ]);
+});
