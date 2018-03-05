be6dbeda6541f3a610aa3ab2325427154c2356fc
Attila Nagy <attila@attilanagy.net>
2018-03-05 15:49:51 +0100

    Cleaning Up gulpfile.js With gulp-load-plugins - Fixes #64

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,22 @@
 'use strict';
 
-var browserSynch = require("browser-sync").create(),
+var $ = require("gulp-load-plugins")(),
+    browserSynch = require("browser-sync").create(),
     ciEnv = process.env.CI ? true : false,
     config = require("./site-config.json"),
-    cleanCSSPlugin = require("postcss-clean"),
-    cssValidator = require("gulp-w3c-css"),
-    cucumber = require("gulp-cucumber"),
+    cleancssPlugin = require("postcss-clean"),
     del = require("del"),
     gulp = require("gulp"),
-    htmlmin = require("gulp-htmlmin"),
-    htmlvalidator = require("gulp-w3cjs"),
-    ico = require('gulp-to-ico'),
     icons = require("simple-icons"),
     mergeStrem = require("merge-stream"),
     noop = require("through2").obj,
-    nunjucks = require("gulp-nunjucks"),
-    postcss = require("gulp-postcss"),
-    sass = require("gulp-sass"),
-    svg2png = require("gulp-svg2png"),
     uncssPlugin = require("postcss-uncss")({ html: [ "build/*.html" ]}),
     variables = { icons: icons, links: config.links, ciEnv: ciEnv },
     variables4Background = { backgroundColor: config.backgroundColor };
+
+gulp.task("pluginDebug", () => {
+  console.log($);
+});
 
 gulp.task("clean", () => {
   return del([ "build/" ]);
@@ -28,47 +24,47 @@ gulp.task("clean", () => {
 
 gulp.task("favicon", () => {
   var svgSource = gulp.src("src/favicon/favicon.svg")
-         .pipe(nunjucks.compile(variables4Background)),
+                  .pipe($.nunjucks.compile(variables4Background)),
       streams = mergeStrem();
 
   config.favicon.dimensions.forEach((d) => {
-    streams.add(svgSource.pipe(svg2png({ width: d, height: d })));
+    streams.add(svgSource.pipe($.svg2png({ width: d, height: d })));
   });
 
   return streams
-         .pipe(ico("favicon.ico"))
+         .pipe($.toIco("favicon.ico"))
          .pipe(gulp.dest("build/"));
 });
 
 gulp.task("html", () => {
   return gulp.src("src/templates/*.html")
-         .pipe(nunjucks.compile(variables, { autoescape: false }))
-         .pipe(ciEnv ? htmlmin(config.htmlmin) : noop())
+         .pipe($.nunjucks.compile(variables, { autoescape: false }))
+         .pipe(ciEnv ? $.htmlmin(config.htmlmin) : noop())
          .pipe(gulp.dest("build/"));
 });
 
 gulp.task("htmlValidator", () => {
   return gulp.src("build/*.html")
-         .pipe(htmlvalidator())
-         .pipe(htmlvalidator.reporter());
+         .pipe($.w3cjs())
+         .pipe($.w3cjs.reporter());
 });
 
 gulp.task("sass", [ "html" ], () => {
   return gulp.src("src/sass/attilanagy.scss")
-         .pipe(nunjucks.compile(variables4Background))
-         .pipe(sass().on("error", sass.logError))
-         .pipe(ciEnv ? postcss([ uncssPlugin, cleanCSSPlugin ]) : noop())
+         .pipe($.nunjucks.compile(variables4Background))
+         .pipe($.sass().on("error", $.sass.logError))
+         .pipe(ciEnv ? $.postcss([ uncssPlugin, cleancssPlugin ]) : noop())
          .pipe(gulp.dest("build/css"));
 });
 
 gulp.task("cssValidator", () => {
   return gulp.src("build/css/*.css")
-         .pipe(cssValidator());
+         .pipe($.w3cCss());
 });
 
 gulp.task("acceptanceTest", () => {
   return gulp.src("src/features/*")
-         .pipe(cucumber({
+         .pipe($.cucumber({
            "steps": "src/features/steps/steps.js",
            "format": "summary"
          }))
